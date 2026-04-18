d6df101ea2e3fbb285975fa10354c57c9feba853
Amr Mesbah <a.mesbah@bayanatz.com>
2026-04-18 12:06:49 +0200

    last update 18 Apr

--- lib/twailo/twilio_constants.dart
+++ lib/twailo/twilio_constants.dart
@@ -1,5 +1,3 @@
 class TwilioConstants {
-  static String twilioAccountSid = 'AC4cee6b33d04e860e5021aaa6b10a14a8';
-  static String twilioAuthToken = '94dfd48b174d237953edc4cf5a78964c';
-  static String twilioVerifyServiceSid = 'VA3720a15fb71b4432f82f1ef11a92ba9b';
+
 }
